0e062739f6dece1a2678d425ef685fe4b2032d0e
Yorqinjon Mahmudov <yorqinjonmahmudov93@gmail.com>
2022-08-07 09:27:05 +0000

    Update Average.java

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -4,9 +4,21 @@ import java.util.Scanner;
 
 public class Average {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // Use Scanner methods to read input
+            Scanner scanner = new Scanner(System.in);
+            // Use Scanner methods to read input
+            int count = 0;
+            int sum = 0;
+            int num;
+            while (true) {
+                num = scanner.nextInt();
+                if (num == 0) {
+                    break;
+                }
+                sum += num;
+                count++;
+            }
 
-    }
+            System.out.println((sum / count));
+        }
 
 }
